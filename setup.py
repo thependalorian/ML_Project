1f11b45c8021098df132348c59cc1d14c5f911c7
George Nekwaya <george@buffr.ai>
2025-05-19 19:44:20 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,30 @@
+# Helps build machine learning project as a package
+
+
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
+setup(
+name='ML_Project',
+version='0.0.1',
+author='George Nekwaya',
+author_email='george@buffr.ai',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
+)
